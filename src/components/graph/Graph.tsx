1e5084b9f8f7b55bdf9f473fd3eac1373fccaa63
Fraser Tooth <frasertooth@gmail.com>
2020-09-13 19:27:32 +0900

    Add target line and legend

--- src/components/graph/Graph.tsx
+++ src/components/graph/Graph.tsx
@@ -6,6 +6,7 @@ import {
   XAxis,
   YAxis,
   Tooltip,
+  Legend,
 } from "recharts";
 
 import {
@@ -20,14 +21,19 @@ import Title from "./Title";
 import useWindowDimensions from "./resize";
 import CustomTooltip, { timeFormatter } from "./Tooltip";
 
+import { useTranslation } from "react-i18next";
+
 const useStyles = makeStyles({
   graphCard: {
     padding: "10px",
   },
 });
 
+const INDUSTRY_TARGET_2030 = 370;
+
 export default function Graph(props: any) {
   const classes = useStyles();
+  const { t } = useTranslation();
 
   const now = new Date();
   const month = now.getMonth();
@@ -42,24 +48,68 @@ export default function Graph(props: any) {
   const { width } = useWindowDimensions();
   const graphWidth = width > 700 ? 500 : width - 100;
 
+  const lineInfo = {
+    today: {
+      color: "#8884d8",
+      type: "line",
+      name: String(t("graph.todayLine")),
+    },
+    compare: {
+      color: "red",
+      type: "line",
+      name: String(t("graph.compareLine")),
+    },
+    target: {
+      color: "green",
+      type: "line",
+      name: String(t("graph.targetLine")),
+    },
+  };
+
+  const legendPayload: ReadonlyArray<any> = [
+    {
+      value: lineInfo.today.name,
+      id: 1,
+      type: "line",
+      color: lineInfo.today.color,
+    },
+    {
+      value: lineInfo.compare.name,
+      id: 2,
+      type: "none",
+      color: lineInfo.compare.color,
+    },
+    {
+      value: lineInfo.target.name,
+      id: 3,
+      type: "line",
+      color: lineInfo.target.color,
+    },
+  ];
+
   if (Object.keys(props.data).length < 12) {
     //Don't render if not enough data yet
     return <CircularProgress />;
   }
-  // Add wrap around for the graph
 
   let data = props.data[monthInAPI][weekdayInAPI];
 
-  if (monthChoice !== month || weekdayChoice !== weekdayInMenu) {
-    const comparisonData = props.data[monthChoice + 1][weekdayChoice + 1];
-    data = data.map((dp: any, i: number) => {
-      return {
-        comparison: comparisonData[i].carbon_intensity,
-        ...dp,
-      };
-    });
-  }
+  data = data.map((dp: any, i: number) => {
+    // Add 2030 target
+    const newDP = {
+      target2030: INDUSTRY_TARGET_2030,
+      ...dp,
+    };
+    // Add Comparison Data to chart and legend, if we have it
+    if (monthChoice !== month || weekdayChoice !== weekdayInMenu) {
+      const comparisonData = props.data[monthChoice + 1][weekdayChoice + 1];
+      newDP.comparison = comparisonData[i].carbon_intensity;
+      legendPayload[1].type = "line";
+    }
+    return newDP;
+  });
 
+  // Copy first Datapoint to the Back, with hour '24' so we get a neat 'midnight to midnight' line
   const adjustedData = JSON.parse(JSON.stringify(data));
   const wrapAround = JSON.parse(JSON.stringify(adjustedData[0]));
   wrapAround.hour = 24;
@@ -67,8 +117,26 @@ export default function Graph(props: any) {
 
   const renderLineChart = (
     <LineChart width={graphWidth} height={300} data={adjustedData}>
-      <Line type="monotone" dataKey="carbon_intensity" stroke="#8884d8" />
-      <Line type="monotone" dataKey="comparison" stroke="red" />
+      <Line
+        name={lineInfo.today.name}
+        type="monotone"
+        dataKey="carbon_intensity"
+        stroke={lineInfo.today.color}
+      />
+      <Line
+        name={lineInfo.compare.name}
+        type="monotone"
+        dataKey="comparison"
+        stroke={lineInfo.compare.color}
+      />
+      <Line
+        name={lineInfo.target.name}
+        type="monotone"
+        dataKey="target2030"
+        stroke={lineInfo.target.color}
+        strokeDasharray="3 3"
+        dot={false}
+      />
       <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
       <XAxis
         dataKey="hour"
@@ -78,8 +146,10 @@ export default function Graph(props: any) {
       />
       <YAxis
         label={{ value: "gC02/kWh", angle: -90, position: "insideLeft" }}
+        domain={[0, 800]}
       />
       <Tooltip content={<CustomTooltip />} />
+      <Legend verticalAlign="bottom" height={36} payload={legendPayload} />
     </LineChart>
   );
 
